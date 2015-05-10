29793628ba3f271814494ce30603762cad9f35a2
szc1523 <szc1523@163.com>
2015-05-10 18:27:54 +0800

    sec1.2

--- cppprimer/cppprimer.cpp
+++ cppprimer/cppprimer.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::cout << "Enter two numbers" << std::endl;
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << "The sum of " << v1 << "and" << v2 << "is" << v1 + v2 << std::endl;
+	
 	return 0;
 }
 
