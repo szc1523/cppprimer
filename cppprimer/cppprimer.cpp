8c53548d061e024565e5cf33044ac9bd6c3d57ab
szc1523 <szc1523@163.com>
2015-05-10 20:37:17 +0800

    finish sec 1.3

--- cppprimer/cppprimer.cpp
+++ cppprimer/cppprimer.cpp
@@ -10,6 +10,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	int v1 = 0, v2 = 0;
 	std::cout << "the sum of " << v1
 	<< "and" << v2;
+	std::cout << "/*";
+	std::cout << /*"*/"/*" /*"*/;
 	return 0;
 }
 
