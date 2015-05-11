85cd029a90f804f223ab45d557365e5277f287e7
szc1523 <szc1523@163.com>
2015-05-11 13:37:19 +0800

    exercise 1.11

--- cppprimer/cppprimer.cpp
+++ cppprimer/cppprimer.cpp
@@ -7,11 +7,22 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int v1 = 0, v2 = 0;
-	std::cout << "the sum of " << v1
-	<< "and" << v2;
-	std::cout << "/*";
-	std::cout << /*"*/"/*" /*"*/;
+	std::cout << "Input Two Integers ";
+	int v1 = 0, v2 = 0, temp = 0;
+	std::cin >> v1 >> v2;
+	if (v1 > v2){
+		temp = v1;
+		v1 = v2;
+		v2 = temp;
+	}
+
+	int i = v1;
+	while (i <= v2){
+		std::cout << " " << i;
+		++i;
+	}
+
+	system("pause");
 	return 0;
 }
 
